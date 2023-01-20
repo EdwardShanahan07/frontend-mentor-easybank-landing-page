bdc9f00dd6b646651e4f72cf6cdca7dd6ed63033
Edward Shanahan <edwardshanahan357@gmail.com>
2023-01-20 22:13:19 +0000

    Add functionality to display navbar list when menu icon is click

--- build/assets/js/index.js
+++ build/assets/js/index.js
@@ -1,5 +1,7 @@
 function toggleMenu() {
   document.querySelector(".menu").classList.toggle("active");
+
+  document.querySelector(".navbar__list").classList.toggle("toggle-list");
 }
 
 document.querySelector(".menu").addEventListener("click", toggleMenu);
