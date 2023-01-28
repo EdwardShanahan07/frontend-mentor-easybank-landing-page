c5b2c4cf5dd82d6fb8401d03a637a1e20d16bb59
Edward Shanahan <edwardshanahan357@gmail.com>
2023-01-28 20:40:13 +0000

    Add JavaScript to toggle navbar list

--- build/assets/js/index.js
+++ build/assets/js/index.js
@@ -1,6 +1,7 @@
 function toggleMenu() {
   document.querySelector(".menu").classList.toggle("active");
   document.querySelector(".overlay").classList.toggle("active");
+  document.querySelector(".navbar__list").classList.toggle("active");
 }
 
 document.querySelector(".menu").addEventListener("click", toggleMenu);
