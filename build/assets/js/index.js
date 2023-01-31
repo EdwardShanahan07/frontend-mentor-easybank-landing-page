26991a1acbfe8420beb095794cb203c3957ae3e7
Edward Shanahan <edwardshanahan357@gmail.com>
2023-01-31 22:04:23 +0000

    Move navbar partial into header partial

--- build/assets/js/index.js
+++ build/assets/js/index.js
@@ -1,7 +1,5 @@
 function toggleMenu() {
-  document.querySelector(".menu").classList.toggle("active");
-  document.querySelector(".overlay").classList.toggle("active");
-  document.querySelector(".navbar__list").classList.toggle("active");
+  document.querySelector("header").classList.toggle("active");
 }
 
 document.querySelector(".menu").addEventListener("click", toggleMenu);
