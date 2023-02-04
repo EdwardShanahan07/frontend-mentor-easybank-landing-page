7ac7b1d9730795f6ca4931ef0197b6cd53dded8d
Edward Shanahan <edwardshanahan357@gmail.com>
2023-02-04 16:50:35 +0000

    Add animation to overlay
    When the menu bars are click, the overlay will appear and disappear when clicked again

--- build/assets/js/index.js
+++ build/assets/js/index.js
@@ -1,5 +1,16 @@
 function toggleMenu() {
-  document.querySelector("header").classList.toggle("active");
+  let headerElement = document.querySelector("header");
+  let overlayElement = document.querySelector(".overlay");
+
+  if (headerElement.classList.contains("open")) {
+    headerElement.classList.remove("open");
+    overlayElement.classList.add("fade-out");
+    overlayElement.classList.remove("fade-in");
+  } else {
+    headerElement.classList.add("open");
+    overlayElement.classList.remove("fade-out");
+    overlayElement.classList.add("fade-in");
+  }
 }
 
 document.querySelector(".menu").addEventListener("click", toggleMenu);
